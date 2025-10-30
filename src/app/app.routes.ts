31edf6a7dd36eab2aa4b13db65f69d54a814dbba
mkeller29 <maxleonardkeller@icloud.com>
2025-10-30 14:40:55 +0100

    Remove login, logout, auth service, register and update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { RidesComponent } from './rides/rides.component';
 import { RideDetailComponent } from './rides/ride-detail/ride-detail.component';
-import { LoginComponent } from './login/login.component'; 
-import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'rides', pathMatch: 'full' },
-    { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent },
     { path: 'rides', component: RidesComponent },
     { path: 'rides/:id', component: RideDetailComponent },
     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] }
