703d43bc7482a9f62036f26063a14c99aa161580
max <maxleonardkeller@icloud.com>
2025-10-31 11:41:36 +0100

    modified apollo provider in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,30 +2,29 @@ import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
-import { APOLLO_OPTIONS } from 'apollo-angular';
-import { createApollo } from './graphql.config';
+//import { AuthInterceptor } from './interceptors/auth.interceptor';
+import {  APOLLO_OPTIONS, provideApollo } from 'apollo-angular';
+//import { createApollo } from './graphql.config';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache } from '@apollo/client/core';
 
+const uri = 'https://carpoolbff-c576f25b03e8.herokuapp.com/graphql';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([AuthInterceptor])),
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: () => {
-        const httpLink = inject(HttpLink);
-        return {
-          cache: new InMemoryCache(),
-          link: httpLink.create({
-            uri: 'https://carpoolbff-c576f25b03e8.herokuapp.com/graphql'
-          })
-        };
-      }
-    }
+    provideHttpClient(),
+    provideApollo(() => {
+     
+      const httpLink = inject(HttpLink); 
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: uri, // <-- Deine API-URL
+        }),
+      };
+    }),
   ]
 };
 
