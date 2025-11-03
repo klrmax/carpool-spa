ee9e2ef9e47f8dc7b9affbfae31075b2efb5cd01
hagjibe01 <hagji.beciri@tecalliance.net>
2025-11-03 19:16:30 +0100

    Show all rides including own rides on /rides page

--- src/app/ride-list/ride-list.component.ts
+++ src/app/ride-list/ride-list.component.ts
@@ -4,7 +4,6 @@ import { RouterLink } from '@angular/router';
 import { RideGraphqlService } from '../services/ride-graphql.serivce';
 import { RideService, Ride } from '../services/ride.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -18,26 +17,9 @@ export class RideListComponent implements OnInit {
   rides$: Observable<Ride[]>;
   loading$: Observable<boolean>;
   error$: Observable<string | null>;
-  private currentUserId: string | null = null;
 
   constructor(private rideService: RideService) {
-    // Hole die aktuelle User-ID
-    if (typeof window !== 'undefined') {
-      this.currentUserId = localStorage.getItem('userid');
-    }
-    
-    // Filtere Rides: Zeige nur Rides von anderen (nicht von mir)
-    this.rides$ = this.rideService.rides$.pipe(
-      map((rides) => {
-        if (!this.currentUserId) return rides;
-        // Filtere heraus: Rides bei denen driver.id === currentUserId
-        return rides.filter(ride => {
-          const driverId = ride.driver?.id?.toString();
-          return driverId !== this.currentUserId;
-        });
-      })
-    );
-    
+    this.rides$ = this.rideService.rides$;
     this.loading$ = this.rideService.loading$;
     this.error$ = this.rideService.error$;
   }
